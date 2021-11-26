be43f35f384ddb57a5b990edf75001a48e3f40fe
consolepy <90243839+consolepy@users.noreply.github.com>
2021-11-26 12:07:06 +0800

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,15 @@
+#Declaring and assigning value to a constant
+
+#constant.py
+
+PI = 3.14
+GRAVITY = 9.8
+
+#main.py
+
+import constant
+
+print(constant.PI)
+print(constant.GRAVITY)
+
+#Make sure constant.py and main.py are in the same directory.
