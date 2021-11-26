e75d812742d83345b9486a7b1b9d3c25b822f58c
consolepy <90243839+consolepy@users.noreply.github.com>
2021-11-26 11:59:33 +0800

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,41 @@
+#Variables
+
+#Storing variables:
+
+number = 10
+
+#Storing variables and replacing them:
+
+number = 10
+number = 1.1
+
+#1. Declaring and assigning a value to a variable:
+
+website = "apple.com"
+print(website)
+
+#Output
+
+#apple.com
+
+#2. Changing the value of a variable
+
+website = "apple.com"
+print(website)
+
+website = "google.com"
+
+print(website)
+
+#Output
+
+#apple.com
+#google.com
+
+#3. Assigning multiple values to multiple variables
+
+a, b, c = 6, 3.1415, "Hewwo"
+
+#If the values of the variables are all the same, then
+
+x = y = z = "same"
